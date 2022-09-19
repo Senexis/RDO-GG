715718e6cbf6ef7740e467b844165e39f31a1b50
Senexis <timoviveen@gmail.com>
2022-09-20 00:10:17 +0200

    Add a bunch of Premium information.

---
+++ src/components/PatreonButtons.js
@@ -0,0 +1,45 @@
+import clsx from 'clsx';
+import React from 'react';
+import useFetch from '../useFetch';
+
+const PatreonButtonsPlaceholder = () => {
+  return (
+    <>
+      <div>
+      {[...Array(3)].map((_, i) => {
+          const items = ['w-32', 'w-36', 'w-40', 'w-44', 'w-48'];
+          const width = items[Math.floor(Math.random() * items.length)];
+          return (
+            <div key={i} className={clsx(width, 'animate-pulse button button--secondary cursor-default m-2 block lg:inline-block')}>&nbsp;</div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const PatreonButtons = () => {
+  const { data, loading, error } = useFetch('https://api.rdo.gg/patrons/');
+
+  if (loading || error || !data) return <PatreonButtonsPlaceholder />;
+
+  return (
+    <>
+      <div className="my-6">
+        {data.tiers.map(tier => {
+          if (!tier.limit || tier.limit.count !== tier.limit.max) {
+            return (
+              <a key={tier.title} href={tier.url} target="_blank" className="button button--secondary m-2 block lg:inline-block">{tier.title} for €{tier.amount_cents / 100}/mo</a>
+            );
+          } else {
+            return (
+              <button disabled key={tier.title} className="button button--secondary m-2 block lg:inline-block bg-transparent" style={{ color: 'var(--ifm-font-color-base)' }}>{tier.title} (Sold out)</button>
+            )
+          }
+        })}
+      </div>
+    </>
+  );
+};
+
+export default PatreonButtons;
